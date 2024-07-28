af7f4c7dafac05f0955febc4b67ca985e9e40a33
Slava Morza <bloodreek@gmail.com>
2024-07-28 18:06:14 +0300

    Init solution

---
+++ LexiLearn/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LexiLearn
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ LexiLearn/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LexiLearn
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
